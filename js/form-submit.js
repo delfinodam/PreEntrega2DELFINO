46af7419825ba9e447333af3b92288f230aeac5c
Damian Delfino <damian.e.delfino@gmail.com>
2023-08-23 17:48:12 -0300

    entrega3

---
+++ js/form-submit.js
@@ -0,0 +1,4 @@
+document.getElementById('send').addEventListener('click', function() {
+    alert('Thanks for your submission! We\'ll contact you soon.');
+    document.getElementById('my-form').reset();
+});
