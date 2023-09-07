87cf874ec15e521836326fb2011f1497af18aa11
Damian Delfino <damian.e.delfino@gmail.com>
2023-09-07 10:02:18 -0300

    Final1

--- js/form-submit.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('send').addEventListener('click', function() {
-    alert('Thanks for your submission! We\'ll contact you soon.');
-    document.getElementById('my-form').reset();
-});
